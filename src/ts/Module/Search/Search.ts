40af43df6eba0c6fd2c2a161188773e382e7eb74
諏訪野ヒロ <supermariomaker1110@gmail.com>
2024-05-28 15:23:26 +0900

    Searchクラス作成

---
+++ src/ts/Module/Search/Search.ts
@@ -0,0 +1,11 @@
+import ModuleBase from "../ModuleBase/ModuleBase.js";
+
+export default class Search extends ModuleBase {
+    constructor() {
+        super();
+    }
+
+    public addEvent(): void {
+
+    }
+}
